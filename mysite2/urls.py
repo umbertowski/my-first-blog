1a22d64c5a17604aeaa17467d60b356f1d521643
umberto <umbertoart7@gmail.com>
2020-04-03 22:38:21 +0200

    ho cambiato l'html

--- mysite2/urls.py
+++ mysite2/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url,include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
